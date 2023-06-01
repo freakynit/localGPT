572a88da9ff13bc9d9ba79a86d793ee80b2c8957
Nitin Bansal <nitin@192.168.1.5>
2023-06-01 09:43:46 +0530

    adding server script

---
+++ server.py
@@ -0,0 +1,90 @@
+from langchain.chains import RetrievalQA
+# from langchain.callbacks.streaming_stdout import StreamingStdOutCallbackHandler
+from langchain.vectorstores import Chroma
+from langchain.embeddings import HuggingFaceInstructEmbeddings
+from langchain.llms import HuggingFacePipeline
+from constants import CHROMA_SETTINGS, PERSIST_DIRECTORY
+from transformers import LlamaTokenizer, LlamaForCausalLM, pipeline
+
+from fastapi import FastAPI
+import argparse
+
+def load_model():
+    '''
+    Select a model on huggingface. 
+    If you are running this for the first time, it will download a model for you. 
+    subsequent runs will use the model from the disk. 
+    '''
+    model_id = "TheBloke/vicuna-7B-1.1-HF"
+    tokenizer = LlamaTokenizer.from_pretrained(model_id)
+
+    model = LlamaForCausalLM.from_pretrained(model_id,
+                                            #   load_in_8bit=True, # set these options if your GPU supports them!
+                                            #   device_map=1#'auto',
+                                            #   torch_dtype=torch.float16,
+                                            #   low_cpu_mem_usage=True
+                                              )
+
+    pipe = pipeline(
+        "text-generation",
+        model=model, 
+        tokenizer=tokenizer, 
+        max_length=2048,
+        temperature=0,
+        top_p=0.95,
+        repetition_penalty=1.15
+    )
+
+    local_llm = HuggingFacePipeline(pipeline=pipe)
+
+    return local_llm
+
+parser = argparse.ArgumentParser(description='Configuration options')
+parser.add_argument('--device_type', type=str, help='device to run on, select gpu or cpu')
+args = parser.parse_args()
+
+device_type = args.device_type
+
+if device_type in ['cpu', 'CPU']:
+    device='cpu'
+else:
+    device='cuda'
+
+print(f"Running on: {device}")
+    
+embeddings = HuggingFaceInstructEmbeddings(model_name="hkunlp/instructor-xl",
+                                            model_kwargs={"device": device})
+db = Chroma(persist_directory=PERSIST_DIRECTORY, embedding_function=embeddings, client_settings=CHROMA_SETTINGS)
+
+retriever = db.as_retriever()
+llm = load_model()
+qa = RetrievalQA.from_chain_type(llm=llm, chain_type="stuff", retriever=retriever, return_source_documents=True)
+
+app = FastAPI()
+
+@app.get("/assistant/")
+def process_query(query: str):
+    res = qa(query)    
+    answer, docs = res['result'], res['source_documents']
+
+    print("\n\n> Question:")
+    print(query)
+    print("\n> Answer:")
+    print(answer)
+    
+    # # Print the relevant sources used for the answer
+    print("----------------------------------SOURCE DOCUMENTS---------------------------")
+    for document in docs:
+        print("\n> " + document.metadata["source"] + ":")
+        print(document.page_content)
+    print("----------------------------------SOURCE DOCUMENTS---------------------------")
+    
+    return answer
+
+if __name__ == "__main__":
+    import uvicorn
+    uvicorn.run(app, host="0.0.0.0", port=8080)
+
+
+if __name__ == "__main__":
+    main()
